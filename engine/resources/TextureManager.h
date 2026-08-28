3092030f5696f319f6ab267d1ad56d9ed5393989
tfindleyjr <105574193+tfindleyjr@users.noreply.github.com>
2026-08-28 12:54:00 -0400

    Phase 14: make texture ownership RAII-safe

--- engine/resources/TextureManager.h
+++ engine/resources/TextureManager.h
@@ -2,14 +2,33 @@
 
 #include <SDL3/SDL.h>
 
+#include <memory>
 #include <string>
 #include <unordered_map>
+
+struct SDLTextureDeleter
+{
+    void operator()(SDL_Texture* texture) const
+    {
+        if (texture)
+        {
+            SDL_DestroyTexture(texture);
+        }
+    }
+};
+
+using TexturePtr = std::unique_ptr<SDL_Texture, SDLTextureDeleter>;
 
 class TextureManager
 {
 public:
-    TextureManager();
-    ~TextureManager();
+    TextureManager() = default;
+    ~TextureManager() = default;
+
+    TextureManager(const TextureManager&) = delete;
+    TextureManager& operator=(const TextureManager&) = delete;
+    TextureManager(TextureManager&&) noexcept = default;
+    TextureManager& operator=(TextureManager&&) noexcept = default;
 
     bool LoadTexture(
         SDL_Renderer* renderer,
@@ -17,15 +36,10 @@ public:
         const std::string& filePath
     );
 
-    SDL_Texture* GetTexture(
-        const std::string& id
-    );
+    SDL_Texture* GetTexture(const std::string& id);
 
     void Shutdown();
 
 private:
-    std::unordered_map<
-        std::string,
-        SDL_Texture*
-    > textures;
+    std::unordered_map<std::string, TexturePtr> textures;
 };
